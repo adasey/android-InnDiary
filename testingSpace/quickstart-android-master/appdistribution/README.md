caa3fc6b7147bf05c9e21eef5ff826940c1a6ce0
Mlink <lmo9903@gmail.com>
2022-06-18 19:25:25 +0900

    google firebase 데모 프로젝트 확인용

---
+++ testingSpace/quickstart-android-master/appdistribution/README.md
@@ -0,0 +1,16 @@
+# Firebase App Distrbution Quickstart
+
+## Introduction
+
+The Firebase App Distribution SDK enables you to display in-app alerts to your testers when new builds of your app are available to install. This quickstart aims to showcase how to use the App Distribution SDK to create and customize new build alerts for your testers. You can read more 
+about Firebase App Distribution [here](https://firebase.google.com/docs/app-distribution)!
+
+## Getting Started
+
+  * Follow the instructions to [add Firebase to your Android app][add-firebase-android].
+
+## Result
+
+<img src="docs/result.png" height="600" />
+
+[add-firebase-android]: https://firebase.google.com/docs/android/setup
---
+++ testingSpace/quickstart-android-master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.google.firebase.quickstart.fcm.kotlin
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.google.firebase.quickstart.fcm.R
+import com.google.firebase.quickstart.fcm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW))
+        }
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        intent.extras?.let {
+            for (key in it.keySet()) {
+                val value = intent.extras?.get(key)
+                Log.d(TAG, "Key: $key Value: $value")
+            }
+        }
+        // [END handle_data_extras]
+
+        binding.subscribeButton.setOnClickListener {
+            Log.d(TAG, "Subscribing to weather topic")
+            // [START subscribe_topics]
+            Firebase.messaging.subscribeToTopic("weather")
+                    .addOnCompleteListener { task ->
+                        var msg = getString(R.string.msg_subscribed)
+                        if (!task.isSuccessful) {
+                            msg = getString(R.string.msg_subscribe_failed)
+                        }
+                        Log.d(TAG, msg)
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    }
+            // [END subscribe_topics]
+        }
+
+        binding.logTokenButton.setOnClickListener {
+            // Get token
+            // [START log_reg_token]
+            Firebase.messaging.getToken().addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val token = task.result
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
+            // [END log_reg_token]
+        }
+
+        Toast.makeText(this, "See README for setup instructions", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+    }
+}
